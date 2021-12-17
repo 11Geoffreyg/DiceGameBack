e79205f4c50d3ac6da60d9f67d3cd43f455523af
Sooseki <bonnetmanon3006@gmail.com>
2021-12-17 12:40:36 +0100

    splitting files to meaning

--- scoring.py
+++ scoring.py
@@ -3,12 +3,10 @@ import random
 NB_DICE_SIDE = 6  # Nb of side of the Dices
 SCORING_DICE_VALUE_LIST = [1, 5]  # List of the side values of the dice who trigger a standard score
 SCORING_MULTIPLIER_LIST = [100, 50]  # List of multiplier for standard score
-DEFAULT_DICES_NB = 5
 
 THRESHOLD_BONUS = 3  # Threshold of the triggering for bonus in term of occurrence of the same slide value
 STD_BONUS_MULTIPLIER = 100  # Standard multiplier for bonus
 ACE_BONUS_MULTIPLIER = 1000  # Special multiplier for aces bonus
-DEFAULT_WINNING_SCORE = 200
 
 
 # return a list of dices value occurrence for a roll of nb_dice_to_roll dices
@@ -72,48 +70,6 @@ def analyse_score(dice_value_occurrence_list, player):
     return bonus_score + standard_score, dice_value_occurrence_list, scoring_dices, bonus_count
 
 
-def nb_dices_left(dice_value_occurrence_list):
-    nb_dices_left_to_play = 0
-
-    for n in dice_value_occurrence_list:
-        nb_dices_left_to_play += n
-
-    return nb_dices_left_to_play
-
-
-def does_player_continue(can_play):
-    if not can_play:
-        return False
-
-    wanna_play = input('Do you want to continue ? (y/n)')
-    while wanna_play != 'y' and wanna_play != 'n':
-        wanna_play = input('Do you want to continue ? (y/n)')
-
-    return True if wanna_play == "y" else False
-
-
-def manage_players():
-    player_count = int(input('How many players ?'))
-    player_list = {}
-    nb_player = 1
-
-    for n in range(player_count):
-        name = input('Player ' + str(nb_player) + ' what is your name ?')
-        player_list[nb_player - 1] = {
-            'name': name,
-            'score': 0,
-            'rolls': 0,
-            'bonus': 0,
-            'lost_points': 0,
-            'no_point_turn': 0,
-            'has_won': False,
-            'full_rolls': 0
-        }
-        nb_player = nb_player + 1
-
-    return player_list
-
-
 def game_stats():
     return {
         'max_turn_score': {
@@ -134,102 +90,6 @@ def game_stats():
     }
 
 
-def has_player_won(score):
-    return score >= DEFAULT_WINNING_SCORE
-
-
-def turn_counter(turn_count):
-    turn_count += 1
-    return turn_count
-
-
-def is_full_role(can_play, score):
-    if not can_play and score > 0:
-        return 1
-    return 0
-
-
-def is_max(number, prev_max):
-    return number > prev_max
-
-
-def game_turn(players, game_stats):
-    has_won = False
-
-    # every player turn
-    for id in players:
-        wanna_play = True
-        player_turn_score = bonus_turn_count = rolls_count_in_turn = 0
-        nb_dices = DEFAULT_DICES_NB
-
-        while wanna_play:
-            wanna_play, player_turn_score, bonus_turn_count, nb_dices, full_roll, rolls_count_in_turn, game_stats = \
-                player_roll(players[id], nb_dices, player_turn_score, bonus_turn_count, rolls_count_in_turn, game_stats)
-
-        # Store player infos
-        players[id]['bonus'] += bonus_turn_count
-        players[id]['score'] += player_turn_score
-        players[id]['full_rolls'] += full_roll
-        players[id]['has_won'] = has_player_won(players[id]['score'])
-        players[id]['rolls'] += rolls_count_in_turn
-
-        if players[id]['has_won']:
-            has_won = True
-
-    return has_won, game_stats, players
-
-
-def player_roll(player, nb_dices, player_turn_score, bonus_turn_count, rolls_count_in_turn, game_stats):
-    input('\n-- ' + player['name'] + ', enter to throw dices.')
-
-    # Game progress : throw dices and get scores
-    dice_value_occurrence_list = roll_dice_set(nb_dices)
-    roll_score, dice_value_occurrence_list, scoring_dices, bonus_count = analyse_score(dice_value_occurrence_list, player)
-    nb_dices = nb_dices_left(dice_value_occurrence_list)
-    can_play = nb_dices_left != 0 and roll_score != 0
-    full_roll = is_full_role(can_play, roll_score)
-    player_turn_score += roll_score
-
-    print('Scoring dices : ', scoring_dices, 'Scoring : ', roll_score, '. You have potentially ', player_turn_score,
-          ' points. You have : ', str(nb_dices), ' dice(s) left to throw.')
-
-    wanna_play = does_player_continue(can_play)
-    bonus_turn_count += bonus_count
-    rolls_count_in_turn += 1
-
-    # Manage points if lost
-    if roll_score == 0:
-        player['lost_points'] += player_turn_score
-        player['no_point_turn'] += 1
-        game_stats['total_no_points_turn'] += 1
-
-        if is_max(player_turn_score, game_stats['max_turn_loss']['score']):
-            game_stats['max_turn_loss'] = {
-                'score': player_turn_score,
-                'player': player['name']
-            }
-        player_turn_score = 0
-    else:
-        if is_max(player_turn_score, game_stats['max_turn_score']['score']):
-            game_stats['max_turn_score'] = {
-                'score': player_turn_score,
-                'player': player['name']
-            }
-
-    if is_max(rolls_count_in_turn, game_stats['longest_turn']['count']):
-        game_stats['longest_turn'] = {
-            'count': rolls_count_in_turn,
-            'player': player['name']
-        }
-
-    return wanna_play, player_turn_score, bonus_turn_count, nb_dices, full_roll, rolls_count_in_turn, game_stats
-
-
-def players_rank(players):
-    players = {k: v for k, v in sorted(players.items(), key=lambda item: item[1]['score'], reverse=True)}
-    return players
-
-
 def calculate_game_stats(players, stats, total_turns_game):
     total_score_game = 0
     total_rolls_game = 0
@@ -244,50 +104,4 @@ def calculate_game_stats(players, stats, total_turns_game):
     stats['mean_non_scoring_turn'] = round((total_lost_points / stats['total_no_points_turn']), 2) if stats['total_no_points_turn'] else 0
 
     return stats
-
-
-def show_stats(players, game_stats, total_turns_game):
-    print('\n Game in : ', total_turns_game, ' turns. ')
-
-    for id in players:
-        has_won = 'win' if players[id]['has_won'] else 'lose'
-
-        # displays for cmd
-        print(players[id]['name'] + ' ' +
-              has_won + ' ! Scoring ' +
-              str(players[id]['score']) + ' in ' +
-              str(players[id]['rolls']) + ' roll(s) with ' +
-              str(players[id]['full_rolls']) + ' full roll. ' +
-              str(players[id]['bonus']) + ' bonus and ' +
-              str(players[id]['lost_points']) + ' potential points lost.')
-
-    # displays general stats for cmd
-    print('\n Mean scoring turn : ' +
-          str(game_stats['mean_scoring_turn']) +
-          ' (' + str(total_turns_game) + ' turn(s)) \n Mean non scoring turn : ' +
-          str(game_stats['mean_non_scoring_turn']) +
-          ' (' + str(game_stats['total_no_points_turn']) + ' turn(s))')
-    print('\nMax score in one turn : ' + str(game_stats['max_turn_score']['score']) + ' by ' + game_stats['max_turn_score']['player'])
-    print('Max loss in one turn : ' + str(game_stats['max_turn_loss']['score']) + ' by ' + game_stats['max_turn_loss']['player'])
-    print('Longest turn : ' + str(game_stats['longest_turn']['count']) + ' by ' + game_stats['longest_turn']['player'])
-
-
-# Manage Game
-def play():
-    players = manage_players()
-    stats = game_stats()
-    has_won = False
-    turn_count = 0
-
-    while not has_won:
-        turn_count = turn_counter(turn_count)
-        has_won, stats, players = game_turn(players, stats)
-
-    players = players_rank(players)
-    stats = calculate_game_stats(players, stats, turn_count)
-    show_stats(players, stats, turn_count)
-
-
-# Init Game
-play()
 
